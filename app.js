ddbed47d975966b3617bd1da178ae6e5997841a2
SeongLimYeom <duatjdfla@naver.com>
2016-02-20 17:11:57 +0900

    first commit

---
+++ app.js
@@ -0,0 +1,12 @@
+var http = require('http');
+var express = require('express');
+var app = express();
+
+app.use(function(req, res){
+	res.writeHead(200, {"Content-Type":"text/html"});
+	res.end('<h1>Hello express</h1>');
+});
+
+http.createServer(app).listen(1337, function(){
+	console.log('Server running ap http://127.0.0.1:1337/');
+});
